afb3238a9ea6215e8c3d3b782abb9769f32c7ca1
DJ-JR30 <djtddy@hotmail.com>
2021-04-28 16:54:13 -0400

    Start

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+
+// -------------------------------------- //
+
+function RndNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+module.exports.RndNumber = RndNumber;
