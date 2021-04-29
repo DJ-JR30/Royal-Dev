e33020c69014bfed62fa10e9346986ae0a0b22c4
DJ-JR30 <djtddy@hotmail.com>
2021-04-28 22:23:28 -0400

    Nsfw ect

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const { Heads, Tails} = require("./Database/imageFlipConfig.json");
+
+
+
+
 function LBtoKG(Value) {
   var poundstoKG = Number(Value) / 2.2;
   return poundstoKG;
@@ -68,18 +73,29 @@ module.exports.generatePassword = generatePassword;
 
 // -------------------------------------- //
 
-function NSFW(Type) {
+function NSFW(Type, Special) {
   if(Type == "ChanelUzi") {
-    var MinUZI = 1;
-    var MaxUZI = 14;
-    var RanNum = Math.floor(Math.random() * (MaxUZI - MinUZI + 1)) + MinUZI;
-    var URL_ChanelUZI = "https://raw.githubusercontent.com/DJ-JR30/MainDB/main/images/ChanelUzi/";
-    var File_Extension = ".jpg";
-    
-    var Combine = URL_ChanelUZI + RanNum + File_Extension;
-    
-    return Combine;
-    
+    if(Special == true) {
+      var MinUZI = 1;
+      var MaxUZI = 30;
+      var RanNum = Math.floor(Math.random() * (MaxUZI - MinUZI + 1)) + MinUZI;
+      var URL_ChanelUZI = "https://raw.githubusercontent.com/DJ-JR30/MainDB/main/images/ChanelUzi/OnlyFans/";
+      var File_Extension = ".jpg";
+      var Combine = URL_ChanelUZI + RanNum + File_Extension;
+      
+      return Combine;
+      
+    }else{
+      var MinUZI = 1;
+      var MaxUZI = 14;
+      var RanNum = Math.floor(Math.random() * (MaxUZI - MinUZI + 1)) + MinUZI;
+      var URL_ChanelUZI = "https://raw.githubusercontent.com/DJ-JR30/MainDB/main/images/ChanelUzi/";
+      var File_Extension = ".jpg";
+      
+      var Combine = URL_ChanelUZI + RanNum + File_Extension;
+      
+      return Combine;
+    }
   }else{
     console.log("NSFW Function having a bad day!");
     console.log("Make sure the type is usable.");
@@ -90,6 +106,31 @@ function NSFW(Type) {
 module.exports.NSFW = NSFW;
 
 // -------------------------------------- //
+
+function NSFWCoin(type) {
+  if (type === 'Normal') {
+    let FrontCoin = Heads;
+    let BackCoin = Tails;
+
+    if (Math.random() < 0.50) {
+      return FrontCoin;
+    } else {
+      return BackCoin;
+    }
+
+    return console.log('Version = Normal');
+  }
+}
+
+
+
+module.exports = {
+  NSFWCoin
+}
+
+
+// ---------------------  //
+
 
 function RndNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
